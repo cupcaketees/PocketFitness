d6f7c55267f8ef168b16f1b808559645bbd00ba7
Michael Small <s6065431@live.tees.ac.uk>
2019-02-14 23:54:41 +0000

    Addition of a post feed front end, with hard-coded sample posts to test the output

---
+++ app/src/main/java/uk/ac/tees/cupcake/feed/Post.java
@@ -0,0 +1,53 @@
+package uk.ac.tees.cupcake.feed;
+
+
+import java.util.ArrayList;
+
+
+
+/**
+ * Created by User on 12/02/2019.
+ */
+
+public class Post  {
+
+    private final String username;
+
+   // private final double rating;
+
+    private final String image;
+    private final String description;
+
+    public Post(String username,  String image, String description){
+        this.username = username;
+        this.image = image;
+        this.description = description;
+    }
+
+    public static ArrayList<Post> createPostsList(int posts) {
+        ArrayList<Post> contacts = new ArrayList<Post>();
+
+        for (int i = 1; i <= posts; i++) {
+            contacts.add(new Post("User " + i, "https://cdn4.vectorstock.com/i/1000x1000/20/68/cartoon-bodybuilder-vector-5592068.jpg",  "Post number " + i));
+        }
+
+        return contacts;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    //public double getRating(){
+      //  return rating;
+    //}
+
+    public String getImage(){
+        return image;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+}
+

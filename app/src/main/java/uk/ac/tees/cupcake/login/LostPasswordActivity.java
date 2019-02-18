22b26e2e6a503fbe6899217ba7d155bca9ffbc10
Bradley Hunter <s6263464@live.tees.ac.uk>
2019-02-18 14:39:15 +0000

    Add completed lost password activity design

---
+++ app/src/main/java/uk/ac/tees/cupcake/login/LostPasswordActivity.java
@@ -0,0 +1,19 @@
+package uk.ac.tees.cupcake.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import uk.ac.tees.cupcake.R;
+
+/**
+ * Lost Password Activity
+ * @author Bradley Hunter <s6263464@tees.ac.uk>
+ */
+
+public class LostPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_password);
+    }
+}

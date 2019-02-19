43ba6ee2c52a2f7537f394cf4e479f178fcb928e
Hugo Tomas <HugoTomas55@hotmail.com>
2019-02-19 17:47:27 +0000

    Add views for the video player. Images added for fullscreen/minimise.

---
+++ app/src/main/java/uk/ac/tees/cupcake/VideoPlayer/VideoPlayerActivity.java
@@ -0,0 +1,17 @@
+package uk.ac.tees.cupcake.VideoPlayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.ac.tees.cupcake.R;
+
+public class VideoPlayerActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+    }
+}

c5e0c66cab8a10545ae0a002ed78cac3f82530a8
Bradley Hunter <s6263464@live.tees.ac.uk>
2019-02-26 09:11:39 +0000

    Add Delete Account Activity

---
+++ app/src/main/java/uk/ac/tees/cupcake/account/DeleteAccountActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.cupcake.account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import uk.ac.tees.cupcake.R;
+
+public class DeleteAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_account);
+    }
+}

bc0501e4e0a83390741e875c71377e0479e8934b
Sam-Hammersley <q5315908@live.tees.ac.uk>
2019-03-01 05:08:53 +0000

    Add graphs to homepage

--- app/src/main/java/uk/ac/tees/cupcake/home/HomeFragment.java
+++ app/src/main/java/uk/ac/tees/cupcake/home/HomeFragment.java
@@ -15,7 +15,6 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.home_fragment, container, false);
     }
 }

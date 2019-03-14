843846bb65dbdf124472ffc1deb79fc5b2949d9e
Hugo Tomas <HugoTomas55@hotmail.com>
2019-03-14 22:48:19 +0000

    User can now upload photo from gallery or take the photo. All information is uploaded to database. The information can be seen in storage and user posts in their profile.

--- app/src/main/java/uk/ac/tees/cupcake/posts/FinalisePost.java
+++ app/src/main/java/uk/ac/tees/cupcake/posts/FinalisePost.java
@@ -1,43 +1,41 @@
 package uk.ac.tees.cupcake.posts;
 
-import android.app.Instrumentation;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.media.Image;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
-import java.util.Date;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
 
 import uk.ac.tees.cupcake.R;
 import uk.ac.tees.cupcake.feed.Post;
+import uk.ac.tees.cupcake.home.HomeActivity;
+import uk.ac.tees.cupcake.utils.IntentUtils;
 
 public class FinalisePost extends AppCompatActivity {
     private static final String TAG = "FinalisePost";
-    private FirebaseAuth mAuth;
     private FirebaseFirestore firebaseFirestore;
     private StorageReference postImages;
     private Bitmap bitmap;
@@ -46,7 +44,11 @@ public class FinalisePost extends AppCompatActivity {
     EditText mText;
     Context context;
     String postPictureURL;
+    Intent intent;
+    ImageView backArrow;
+    TextView shareButton;
 
+    Uri uri;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -54,12 +56,14 @@ public class FinalisePost extends AppCompatActivity {
         setContentView(R.layout.activity_finalise_post);
 
         firebaseFirestore = FirebaseFirestore.getInstance();
-        mAuth = FirebaseAuth.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
         context = FinalisePost.this;
         postImages = FirebaseStorage.getInstance().getReference().child("Posts Images");
 
         mText = findViewById(R.id.finaliseDescription);
-
+        shareButton = findViewById(R.id.postFinalise);
+        backArrow = findViewById(R.id.backArrow);
+        intent = getIntent();
         mCurrentUserId = mAuth.getCurrentUser().getUid();
 
         imageView = findViewById(R.id.imageShare);
@@ -70,81 +74,71 @@ public class FinalisePost extends AppCompatActivity {
     }
 
     private void initialise() {
-        ImageView backArrow = findViewById(R.id.backArrow);
+
         backArrow.setOnClickListener(v -> finish());
 
-        TextView post = findViewById(R.id.postFinalise);
-        post.setOnClickListener(v -> {
+        shareButton.setOnClickListener(v -> {
 
-//
 
-//
-//            if(TextUtils.isEmpty(mText.toString().trim())){
-//                Toast.makeText(FinalisePost.this, validateInput(), Toast.LENGTH_SHORT).show();
-//                return;
-//            }
+            backArrow.setEnabled(false);
+            shareButton.setClickable(false);
+            mText.setClickable(false);
 
-            StorageReference userFilePath = postImages.child(mCurrentUserId).child("1.jpg");
+            ProgressBar shareProgress = findViewById(R.id.shareProgressBar);
+            shareProgress.setVisibility(View.VISIBLE);
 
-            Uri uri =  getImageUri(FinalisePost.this, bitmap);
+            StorageReference userFilePath = postImages.child(mCurrentUserId).child(Post.getCurrentTimeUsingDate() + ".jpg");
 
-            userFilePath.putFile(uri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                    if(task.isSuccessful()){
 
-                        Toast.makeText(FinalisePost.this, "image uploaded", Toast.LENGTH_SHORT).show();
+            if (intent.hasExtra("Selected_image")) {
+                String imageURL = intent.getStringExtra("Selected_image");
+                Bitmap bm = convertToBitmap(imageURL);
 
-                        postPictureURL = task.getResult().getDownloadUrl().toString();
-                        savePost();
+                uri = getImageUri(FinalisePost.this, bm);
+            } else {
+                uri = getImageUri(FinalisePost.this, bitmap);
 
-                    }else{
-                        Toast.makeText(FinalisePost.this, "image not uploaded", Toast.LENGTH_SHORT).show();
-                        //todo
-                    }
+
+            }
+
+            userFilePath.putFile(uri).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+
+                    Toast.makeText(FinalisePost.this, "image uploaded", Toast.LENGTH_SHORT).show();
+
+                    postPictureURL = task.getResult().getDownloadUrl().toString();
+                    savePost();
+
+                } else {
+                    Toast.makeText(FinalisePost.this, "image not uploaded", Toast.LENGTH_SHORT).show();
                 }
-            });
 
+                backArrow.setEnabled(true);
+                shareButton.setClickable(true);
+                mText.setClickable(true);
+                shareProgress.setVisibility(View.INVISIBLE);
+            });
         });
     }
-
-
 
     private void savePost() {
-        Post post = new Post(mCurrentUserId, postPictureURL, mText.toString(), Post.getCurrentTimeUsingDate());
-
-        firebaseFirestore.collection("Users").document(mCurrentUserId).collection("User Posts").document("post1").set(post).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show();
-
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
-            }
-        });
+        Post post = new Post(mCurrentUserId, postPictureURL, mText.getText().toString(), Post.getCurrentTimeUsingDate());
 
 
-
-
+        firebaseFirestore.collection("Users").document(mCurrentUserId).collection("User Posts").document(post.getDate()).set(post)
+                .addOnSuccessListener(aVoid -> IntentUtils.invokeBaseView(FinalisePost.this, HomeActivity.class))
+                .addOnFailureListener(e -> Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show());
     }
-
-
 
 
     private void defineImage() {
-        Intent intent = getIntent();
-
 
         String mAppend = "file:/";
-              if(intent.hasExtra("selected_image")) {
-            UniversalImageLoader.setImage(intent.getStringExtra("Selected_image"), imageView , null, mAppend);
+        if (intent.hasExtra("Selected_image")) {
+            UniversalImageLoader.setImage(intent.getStringExtra("Selected_image"), imageView, null, mAppend);
         } else {
             bitmap = intent.getParcelableExtra("selected_bitmap");
             imageView.setImageBitmap(bitmap);
-
 
         }
 
@@ -157,8 +151,24 @@ public class FinalisePost extends AppCompatActivity {
         return Uri.parse(path);
     }
 
+    public Bitmap convertToBitmap(String imageURL) {
+        File imageFile = new File(imageURL);
+        FileInputStream fis = null;
+        Bitmap bitmap = null;
 
-
-
+        try {
+            fis = new FileInputStream(imageFile);
+            bitmap = BitmapFactory.decodeStream(fis);
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "convertToBitmap:  FileNotFoundException " + e.getMessage());
+        } finally {
+            try {
+                Objects.requireNonNull(fis).close();
+            } catch (IOException e) {
+                Log.e(TAG, "convertToBitmap:  FileNotFoundException " + e.getMessage());
+            }
+        }
+        return bitmap;
+    }
 
 }

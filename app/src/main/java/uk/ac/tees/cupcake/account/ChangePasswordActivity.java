1cc431114fc43bd82d6151cf67a4748e8bcb63f3
Bradley Hunter <s6263464@live.tees.ac.uk>
2019-03-15 00:07:12 +0000

    refactored change password activity java

--- app/src/main/java/uk/ac/tees/cupcake/account/ChangePasswordActivity.java
+++ app/src/main/java/uk/ac/tees/cupcake/account/ChangePasswordActivity.java
@@ -9,8 +9,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,7 +26,7 @@ import uk.ac.tees.cupcake.home.HomeActivity;
 
 public class ChangePasswordActivity extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
+    private FirebaseAuth mAuth;
     private EditText mPasswordEditText;
     private EditText mNewPasswordEditText;
 
@@ -34,56 +34,72 @@ public class ChangePasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
-
         setTitle("Change Password");
+
         mAuth = FirebaseAuth.getInstance();
         mPasswordEditText = findViewById(R.id.change_password_password_edit_text);
         mNewPasswordEditText = findViewById(R.id.change_password_new_password_edit_text);
     }
 
     /*
-     * Authenticate current user using password input, on success attempts to change user password with input provided.
+     * Attempts to re authenticate current user, requires current user password and new password input value from user.
+     * On success changes current user password to mNewPasswordEditText value and returns user to home activity.
+     * On failure prompts user with appropriate message.
      */
     public void changePassword(View view){
 
-        String userInputPassword = mPasswordEditText.getText().toString().trim();
-        String userInputNewPassword = mNewPasswordEditText.getText().toString().trim();
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
-        if(TextUtils.isEmpty(userInputPassword)) {
-            Toast.makeText(ChangePasswordActivity.this, "You must enter your password", Toast.LENGTH_SHORT).show();
+        String userInputCurrentPassword = mPasswordEditText.getText().toString().trim();
+        String userInputNewPassword = mNewPasswordEditText.getText().toString().trim();
+
+        String result = validateUserInput(userInputCurrentPassword, userInputNewPassword);
+
+        if(!result.isEmpty()){
+            Toast.makeText(ChangePasswordActivity.this, result, Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if(TextUtils.isEmpty(userInputNewPassword)) {
-            Toast.makeText(ChangePasswordActivity.this, "You must enter your new password", Toast.LENGTH_SHORT).show();
-            return;
-        }
+        AuthCredential credential = EmailAuthProvider.getCredential(mAuth.getCurrentUser().getEmail(), userInputCurrentPassword);
 
-        AuthCredential credential = EmailAuthProvider.getCredential(mAuth.getCurrentUser().getEmail(), userInputPassword);
-
-        currentUser.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+        currentUser.reauthenticate(credential).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
-                    currentUser.updatePassword(userInputNewPassword)
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()){
-                                        Toast.makeText(ChangePasswordActivity.this, "Your password has been updated", Toast.LENGTH_SHORT).show();
-                                        startActivity(new Intent(ChangePasswordActivity.this, HomeActivity.class));
-                                    }else{
-                                        String errorMessage = task.getException().getMessage();
-                                        Toast.makeText(ChangePasswordActivity.this, "Error:" + errorMessage, Toast.LENGTH_LONG).show();
-                                    }
-                                }
-                            });
-                }else{
-                    String errorMessage = task.getException().getMessage();
-                    Toast.makeText(ChangePasswordActivity.this, "Error:" + errorMessage, Toast.LENGTH_LONG).show();
-                }
+            public void onSuccess(Void aVoid) {
+
+                currentUser.updatePassword(userInputNewPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(ChangePasswordActivity.this, "Your password has been changed successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(ChangePasswordActivity.this, HomeActivity.class));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ChangePasswordActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ChangePasswordActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    /*
+     * Validates input values passed are not empty.
+     * Returns empty string on success, otherwise a string with appropriate message.
+     */
+    private String validateUserInput(String userInputCurrentPassword, String userInputNewPassword){
+        StringBuilder sb = new StringBuilder();
+
+        if(TextUtils.isEmpty(userInputCurrentPassword)) {
+            sb.append("You must enter your current password. ");
+        }
+        if(TextUtils.isEmpty(userInputNewPassword)) {
+            sb.append("You must enter your new password.");
+        }
+        return sb.toString();
     }
 }

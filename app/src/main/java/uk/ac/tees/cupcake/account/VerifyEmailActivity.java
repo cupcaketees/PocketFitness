cd147713890b87178ab9217ceff06d04c6f3e482
Bradley Hunter <s6263464@live.tees.ac.uk>
2019-03-18 00:28:00 +0000

    Refactored VerifyEmailActivity java

--- app/src/main/java/uk/ac/tees/cupcake/account/VerifyEmailActivity.java
+++ app/src/main/java/uk/ac/tees/cupcake/account/VerifyEmailActivity.java
@@ -1,57 +1,42 @@
 package uk.ac.tees.cupcake.account;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import uk.ac.tees.cupcake.R;
 import uk.ac.tees.cupcake.home.HomeActivity;
 
-/*
+/**
  * Verify Email Address Activity
- * @author Bradley Hunter <s6263464@tees.ac.uk>
+ * @author Bradley Hunter <s6263464@live.tees.ac.uk>
  */
-
 public class VerifyEmailActivity extends AppCompatActivity {
-
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_email);
         setTitle("Verify Email Address");
-
-        mAuth = FirebaseAuth.getInstance();
     }
 
-    /*
+    /**
      * On success sends instructions to verify email address to current user registered email address.
      * On failure prompts user with appropriate message.
      */
-    public void verifyEmail(View view){
+    public void verifyEmail(View view) {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-        currentUser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Toast.makeText(VerifyEmailActivity.this, "Instructions have been sent to your registered email address", Toast.LENGTH_LONG).show();
-                startActivity(new Intent(VerifyEmailActivity.this, HomeActivity.class));
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(VerifyEmailActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
+        currentUser.sendEmailVerification()
+                   .addOnSuccessListener(aVoid -> {
+                       Toast.makeText(VerifyEmailActivity.this, "Instructions have been sent to your registered email address", Toast.LENGTH_SHORT).show();
+                       startActivity(new Intent(VerifyEmailActivity.this, HomeActivity.class));
+                   })
+                   .addOnFailureListener(e -> Toast.makeText(VerifyEmailActivity.this, e.getMessage(), Toast.LENGTH_LONG).show());
     }
 }

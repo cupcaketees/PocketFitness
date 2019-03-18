8bf9352b305f5f21296807bd2043298bc342bc77
Bradley Hunter <s6263464@live.tees.ac.uk>
2019-03-18 00:29:34 +0000

    Refactored profile page java

--- app/src/main/java/uk/ac/tees/cupcake/account/ProfilePageActivity.java
+++ app/src/main/java/uk/ac/tees/cupcake/account/ProfilePageActivity.java
@@ -9,103 +9,74 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
 import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import uk.ac.tees.cupcake.R;
 
-/*
- * Profile Page
- * @author Bradley Hunter <s6263464@tees.ac.uk>
+/**
+ * Profile Page Activity
+ * @author Bradley Hunter <s6263464@live.tees.ac.uk>
  */
-
 public class ProfilePageActivity extends AppCompatActivity {
 
-    private TextView mProfileNameTextView;
-    private CircleImageView mProfilePictureImageView;
-    private ImageView mCoverPhotoImageView;
-
-    private TextView mEmailAddressTextView;
-    private TextView mAccountCreatedTextView;
-
     private FirebaseAuth mAuth;
-    private String mCurrentUserUid;
-    private ListenerRegistration profilePageListener;
+    private ListenerRegistration mProfilePageListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
 
-        mProfileNameTextView = findViewById(R.id.profile_name_text_view);
-        mProfilePictureImageView = findViewById(R.id.profile_profile_image_image_view);
-        mEmailAddressTextView = findViewById(R.id.profile_email_text_view);
-        mAccountCreatedTextView = findViewById(R.id.profile_account_created_text_view);
-        mCoverPhotoImageView = findViewById(R.id.edit_profile_cover_image_view);
-
         mAuth = FirebaseAuth.getInstance();
-
-        mCurrentUserUid = mAuth.getCurrentUser().getUid();
-        mEmailAddressTextView.setText(mAuth.getCurrentUser().getEmail());
     }
 
-    /*
-     * Updates profile page to any changes made to current user document while user is on the page.
+    /**
+     * Listener immediately gets data from current user document and sets profile page to values
      */
     @Override
     protected void onStart(){
         super.onStart();
+        String currentUserUid = mAuth.getCurrentUser().getUid();
 
-        profilePageListener = FirebaseFirestore.getInstance().collection("Users").document(mCurrentUserUid).addSnapshotListener(new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
-                if(e != null){
-                    Toast.makeText(ProfilePageActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
-                    return;
-                }
+        FirebaseFirestore.getInstance()
+                         .collection("Users")
+                         .document(currentUserUid)
+                         .addSnapshotListener(this, (documentSnapshot, e) -> {
+                             // In case something went wrong while loading
+                             if(e != null){
+                                 Toast.makeText(ProfilePageActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                                 return;
+                             }
 
-                if(documentSnapshot.exists()){
-                    UserProfile userProfile = documentSnapshot.toObject(UserProfile.class);
-                    String firstName = userProfile.getFirstName();
-                    String lastName = userProfile.getLastName();
-                    String profileImage = userProfile.getProfileImageUrl();
-                    String coverImage = userProfile.getCoverPhotoUrl();
+                             if(documentSnapshot.exists()){
+                                 TextView profileNameTextView = findViewById(R.id.profile_name_text_view);
+                                 TextView accountCreatedTextView = findViewById(R.id.profile_account_created_text_view);
+                                 TextView emailAddressTextView = findViewById(R.id.profile_email_text_view);
+                                 CircleImageView profileImageView = findViewById(R.id.profile_profile_image_image_view);
+                                 ImageView coverPhotoImageView = findViewById(R.id.profile_cover_image_image_view);
 
-                    mProfileNameTextView.setText(firstName + " " + lastName);
-                    mAccountCreatedTextView.setText(userProfile.getAccountCreated());
+                                 UserProfile userProfile = documentSnapshot.toObject(UserProfile.class);
 
-                    if(profileImage != null){
-                        Picasso.with(ProfilePageActivity.this).load(profileImage).into(mProfilePictureImageView);
-                    }
+                                 if(userProfile.getProfilePictureUrl() != null){
+                                     Picasso.with(ProfilePageActivity.this).load(userProfile.getProfilePictureUrl()).into(profileImageView);
+                                 }
 
-                    if(coverImage != null){
-                        //Picasso.with(ProfilePageActivity.this).load(coverImage).into(mCoverPhotoImageView);
-                    }
+                                 if(userProfile.getCoverPhotoUrl() != null){
+                                     Picasso.with(ProfilePageActivity.this).load(userProfile.getCoverPhotoUrl()).into(coverPhotoImageView);
+                                 }
 
-                }
-            }
-        });
+                                 profileNameTextView.setText(userProfile.getFirstName() + " " + userProfile.getLastName());
+                                 emailAddressTextView.setText(mAuth.getCurrentUser().getEmail());
+                                 accountCreatedTextView.setText("Member since " + userProfile.getAccountCreated());
+                             }
+                         });
     }
 
-    /*
-     * Sends user to edit profile activity
+    /**
+     * Intent to edit profile activity
      */
-    public void editProfile(View view){
-        startActivity(new Intent(ProfilePageActivity.this,EditProfileActivity.class));
-    }
-
-    /*
-     * Removes profile page listener
-     */
-    @Override
-    protected void onStop(){
-        super.onStop();
-        profilePageListener.remove();
-    }
-
+    public void editProfileOnClick(View view){ startActivity(new Intent(ProfilePageActivity.this,EditProfileActivity.class)); }
 }

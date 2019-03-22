b1c565405e4bbdf7249a9a290796376ef65a4dbe
Bradley Hunter <s6263464@live.tees.ac.uk>
2019-03-22 03:57:16 +0000

    add search food xml

---
+++ app/src/main/java/uk/ac/tees/cupcake/food/SearchFoodActivity.java
@@ -0,0 +1,16 @@
+package uk.ac.tees.cupcake.food;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import uk.ac.tees.cupcake.R;
+
+public class SearchFoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_food);
+    }
+
+
+}

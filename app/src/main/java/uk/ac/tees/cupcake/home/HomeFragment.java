353e2525dae157a5a1d9e61ac95716e8808e914a
Sam-Hammersley <q5315908@live.tees.ac.uk>
2019-03-24 00:09:43 +0000

    Improve home fragment layout

--- app/src/main/java/uk/ac/tees/cupcake/home/HomeFragment.java
+++ app/src/main/java/uk/ac/tees/cupcake/home/HomeFragment.java
@@ -9,7 +9,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,20 +23,19 @@ import com.github.mikephil.charting.formatter.DefaultValueFormatter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
-import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
-import de.hdodenhof.circleimageview.CircleImageView;
 import uk.ac.tees.cupcake.R;
-import uk.ac.tees.cupcake.account.UserProfile;
 import uk.ac.tees.cupcake.home.health.heartrate.HeartRateMeasurement;
 import uk.ac.tees.cupcake.sensors.SensorAdapter;
 import uk.ac.tees.cupcake.sensors.StepCounterSensorListener;
 
-public class HomeFragment extends Fragment {
+public final class HomeFragment extends OnChangeFragment {
     
     private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
     
@@ -51,21 +49,25 @@ public class HomeFragment extends Fragment {
     
     private TextView lastMeasurement;
     
+    private TextView lastMeasurementDate;
+    
+    private TextView lastMeasurementType;
+    
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        
-        View view = inflater.inflate(R.layout.home_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
         
         barChart = view.findViewById(R.id.home_steps_bar_chart);
         setupBarChart(barChart);
         
         stepsCard = view.findViewById(R.id.home_steps_card);
         lastMeasurement = view.findViewById(R.id.home_heart_last_measurement);
-        
+        lastMeasurementDate = view.findViewById(R.id.home_heart_last_measurement_date);
+        lastMeasurementType = view.findViewById(R.id.home_heart_last_measurement_type);
+    
         return view;
     }
-    
     
     @Override
     public void onStart() {
@@ -76,9 +78,14 @@ public class HomeFragment extends Fragment {
             Toast.makeText(getContext(), "Failed to setup sensors", Toast.LENGTH_SHORT).show();
         }
         sensorAdapter.registerListener(SensorManager.SENSOR_DELAY_NORMAL, Sensor.TYPE_STEP_COUNTER, new StepCounterSensorListener(stepsCard));
-    
+        
         getStepsData(barChart);
-        getHeartData(lastMeasurement);
+        getHeartData(lastMeasurement, lastMeasurementType, lastMeasurementDate);
+    }
+    
+    @Override
+    public void onChange() {
+        barChart.animateY(1000, Easing.Linear);
     }
     
     private void setupBarChart(BarChart barChart) {
@@ -114,49 +121,51 @@ public class HomeFragment extends Fragment {
     }
     
     private void getStepsData(final BarChart barChart) {
-        if (auth.getCurrentUser() != null) {
-            firestore.collection("UserStats")
-                    .document(auth.getCurrentUser().getUid())
-                    .collection("StepCounts")
-                    .limit(7)
-                    .get()
-                    .addOnSuccessListener(documentSnapshots -> {
-                        List<StepCountMeasurement> measurements = documentSnapshots.toObjects(StepCountMeasurement.class);
-                        List<BarEntry> entries = new ArrayList<>(measurements.size());
-
-                        for (int index = 0; index < measurements.size(); index++) {
-                            StepCountMeasurement measurement = measurements.get(index);
-
-                            BarEntry barEntry = new BarEntry(measurement.getTimestamp().getDay(), measurement.getCount());
-
-                            entries.add(barEntry);
-                        }
-
-                        setChartData(barChart, entries);
-                        barChart.animateY(1000, Easing.Linear);
-                    })
-                    .addOnFailureListener(e -> Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show());
-        }
+        firestore.collection("UserStats")
+                .document(auth.getCurrentUser().getUid())
+                .collection("StepCounts")
+                .orderBy("timestamp", Query.Direction.DESCENDING)
+                .limit(7)
+                .get()
+                .addOnSuccessListener(documentSnapshots -> {
+                    List<StepCountMeasurement> measurements = documentSnapshots.toObjects(StepCountMeasurement.class);
+                    List<BarEntry> entries = new ArrayList<>(measurements.size());
+                    
+                    for (int index = 0; index < measurements.size(); index++) {
+                        StepCountMeasurement measurement = measurements.get(index);
+                        
+                        BarEntry barEntry = new BarEntry(measurement.getTimestamp().getDay(), measurement.getCount());
+    
+                        entries.add(barEntry);
+                    }
+                    
+                    setChartData(barChart, entries);
+                })
+                .addOnFailureListener(e -> Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show());
     }
     
-    private void getHeartData(final TextView textView) {
-        if (auth.getCurrentUser() != null) {
-            firestore.collection("UserStats")
-                    .document(auth.getCurrentUser().getUid())
-                    .collection("HeartRates")
-                    .orderBy("timestamp", Query.Direction.DESCENDING)
-                    .limit(1)
-                    .get()
-                    .addOnSuccessListener(documentSnapshots -> {
-                        List<HeartRateMeasurement> measurements = documentSnapshots.toObjects(HeartRateMeasurement.class);
-
-                        if (!measurements.isEmpty()) {
-
-                            textView.setText(getResources().getString(R.string.heart_rate_text, measurements.get(0).getBpm()));
-                        }
-                    })
-                    .addOnFailureListener(e -> Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show());
-        }
+    private void getHeartData(TextView value, TextView type, TextView date) {
+        firestore.collection("UserStats")
+                .document(auth.getCurrentUser().getUid())
+                .collection("HeartRates")
+                .orderBy("timestamp", Query.Direction.DESCENDING)
+                .limit(1)
+                .get()
+                .addOnSuccessListener(documentSnapshots -> {
+                    List<HeartRateMeasurement> measurements = documentSnapshots.toObjects(HeartRateMeasurement.class);
+    
+                    if (!measurements.isEmpty()) {
+                        HeartRateMeasurement measurement = measurements.get(0);
+                        String measurementType = measurement.getMeasurementType();
+    
+                        value.setText(getResources().getString(R.string.heart_rate_text, measurement.getBpm()));
+                        type.setText(Character.toUpperCase(measurementType.charAt(0)) + measurementType.substring(1));
+                        date.setText(DATE_FORMAT.format(new Date(measurement.getTimestamp())));
+                    }
+                })
+                .addOnFailureListener(e -> Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show());
     }
+    
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd MMM yyyy HH:mm");
     
 }

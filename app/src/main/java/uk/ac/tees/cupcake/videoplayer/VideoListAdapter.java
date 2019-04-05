a423b8ed1a9baa93bc842a40806ccf182e2dbb69
s6065431 <s6065431@tees.ac.uk>
2019-04-05 10:18:58 +0100

    Add no results found

--- app/src/main/java/uk/ac/tees/cupcake/videoplayer/VideoListAdapter.java
+++ app/src/main/java/uk/ac/tees/cupcake/videoplayer/VideoListAdapter.java
@@ -2,6 +2,7 @@ package uk.ac.tees.cupcake.videoplayer;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,10 @@ import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.List;
 import uk.ac.tees.cupcake.R;
@@ -19,7 +24,7 @@ import uk.ac.tees.cupcake.utils.IntentUtils;
  * @author Hugo Tomas <s6006225@live.tees.ac.uk>
  */
 class VideoListAdapter extends RecyclerView.Adapter<VideoListAdapter.ViewHolder> implements Filterable {
-
+    private static final String TAG = "VideoListAdapter";
     private ArrayList<Video> mVideos;
     private final Context mContext;
     private ArrayList<Video> mVideosAll;
@@ -52,6 +57,16 @@ class VideoListAdapter extends RecyclerView.Adapter<VideoListAdapter.ViewHolder>
         holder.mTime.setText(mVideos.get(position).getTime());
 
         holder.itemView.setOnClickListener(v -> IntentUtils.invokeVideoView(mContext, VideoPlayerActivity.class, "VIDEO_NAME", mVideos.get(position).getExtra()));
+
+        if(holder.mName.getText() == "NO RESULTS FOUND...") {
+            holder.itemView.setClickable(false);
+            holder.mTime.setVisibility(View.GONE);
+            holder.mImageView.setVisibility(View.GONE);
+        } else {
+            holder.itemView.setClickable(true);
+            holder.mTime.setVisibility(View.VISIBLE);
+            holder.mImageView.setVisibility(View.VISIBLE);
+        }
     }
 
     /**
@@ -82,6 +97,10 @@ class VideoListAdapter extends RecyclerView.Adapter<VideoListAdapter.ViewHolder>
                         filteredList.add(item);
                     }
                 }
+                if (filteredList.isEmpty()) {
+                    filteredList.add(new Video("NO RESULTS FOUND...", "",R.drawable.temp_man_running,"","",""));
+
+                }
             }
             FilterResults results = new FilterResults();
             results.values = filteredList;
@@ -106,6 +125,7 @@ class VideoListAdapter extends RecyclerView.Adapter<VideoListAdapter.ViewHolder>
         private TextView mDescription;
         private ImageView mImageView;
         private TextView mTime;
+        public TextView mNoResults;
 
         ViewHolder(View itemView) {
             super(itemView);
@@ -114,6 +134,7 @@ class VideoListAdapter extends RecyclerView.Adapter<VideoListAdapter.ViewHolder>
             mDescription = itemView.findViewById(R.id.post_desc);
             mImageView = itemView.findViewById(R.id.post_image);
             mTime = itemView.findViewById(R.id.post_time);
+            mNoResults = itemView.findViewById(R.id.no_results);
         }
 
     }

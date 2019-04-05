3130e501cb4ef9345f1c4290ce0dd1ffbf013ff9
Bradley Hunter <s6263464@live.tees.ac.uk>
2019-04-05 01:18:00 +0100

    Refactoring of lost password activity.

--- app/src/main/java/uk/ac/tees/cupcake/login/LostPasswordActivity.java
+++ app/src/main/java/uk/ac/tees/cupcake/login/LostPasswordActivity.java
@@ -2,142 +2,86 @@ package uk.ac.tees.cupcake.login;
 
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
-import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-
 import uk.ac.tees.cupcake.R;
-import uk.ac.tees.cupcake.home.MainActivity;
 
 /**
  * Lost Password Activity
  * @author Bradley Hunter <s6263464@tees.ac.uk>
  */
-
 public class LostPasswordActivity extends AppCompatActivity {
-
-    private ConstraintLayout mLostPasswordBackground;
-    private AnimationDrawable mBackgroundAnimation;
-
-    private EditText mEmailEditText;
-    private Button mResetPasswordButton, mSignInButton;
-
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost_password);
 
-        // Initialise fields
-        mAuth = FirebaseAuth.getInstance();
-        mEmailEditText = findViewById(R.id.lost_password_email_edit_text);
-        mResetPasswordButton = findViewById(R.id.lost_password_reset_password_button);
-        mSignInButton = findViewById(R.id.lost_password_sign_in_button);
-
         // Call method to start background animation.
         initBackground();
-
-        /*
-         * Checks if user is logged in and updates UI accordingly.
-         */
-        mAuthListener = firebaseAuth -> {
-            if(firebaseAuth.getCurrentUser() != null){
-                sendUserToHomeActivity();
-            }
-        };
-
-        /*
-         * Sends user to Login Activity
-         */
-        mSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendUserToLoginActivity();
-            }
-        });
-
-        /*
-         * Calls reset password method
-         */
-        mResetPasswordButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetUserPassword();
-            }
-        });
     }
 
-    /*
-     * Validates user input and attempts sends password instructions to email.
+    /**
+     * On click method call to send user to login activity when go to login activity is pressed.
+     */
+    public void loginActivityOnClick(View view){
+        sendUserToActivity(LoginActivity.class);
+    }
+
+    /**
+     * On click call method call to start recovering lost password process.
+     */
+    public void resetUserPasswordOnClick(View view){
+        resetUserPassword();
+    }
+    /**
+     * Checks input is not null, attempts to send password reset instructions.
+     * On success return user to login activity
+     * On failure prompts user with appropriate message.
      */
     private void resetUserPassword(){
-        String userInputEmail = mEmailEditText.getText().toString().trim();
-        // Checks input field is not empty
-        if(TextUtils.isEmpty(userInputEmail)){
-            Toast.makeText(LostPasswordActivity.this, "You must enter your email address", Toast.LENGTH_SHORT).show();
-        }else{
-            mAuth.sendPasswordResetEmail(userInputEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    if(task.isSuccessful()){
-                        Toast.makeText(LostPasswordActivity.this,"Password reset instructions have been sent to your email", Toast.LENGTH_SHORT).show();
-                        sendUserToLoginActivity();
-                    }else{
-                        String errorMessage = task.getException().getMessage();
-                        Toast.makeText(LostPasswordActivity.this,"Password Reset Failed : " + errorMessage, Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
+        EditText emailEditText = findViewById(R.id.lost_password_email_edit_text);
+        String userInputEmail = emailEditText.getText().toString().trim();
+
+        if(TextUtils.isEmpty(userInputEmail)) {
+            Toast.makeText(LostPasswordActivity.this, "You must enter your email address.", Toast.LENGTH_SHORT).show();
+            return;
         }
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        auth.sendPasswordResetEmail(userInputEmail)
+            .addOnCompleteListener(task -> {
+                Toast.makeText(LostPasswordActivity.this, "Reset instructions have been sent to your email address.", Toast.LENGTH_SHORT).show();
+                sendUserToActivity(LoginActivity.class);
+            })
+            .addOnFailureListener(e -> Toast.makeText(LostPasswordActivity.this, e.getMessage(), Toast.LENGTH_LONG).show());
     }
 
-    /*
-    * Send user to login activity.
-    */
-    private void sendUserToLoginActivity(){
-        Intent loginIntent = new Intent(LostPasswordActivity.this, LoginActivity.class);
-        startActivity(loginIntent);
-        finish();
-    }
-
-    /*
-     * Send user to home activity.
+    /**
+     * Helper method to create intent to destination passed through params.
      */
-    private void sendUserToHomeActivity(){
-        Intent loginIntent = new Intent(LostPasswordActivity.this, MainActivity.class);
-        startActivity(loginIntent);
-        finish();
+    private void sendUserToActivity(Class dest){
+        startActivity(new Intent(LostPasswordActivity.this, dest));
     }
 
-    /*
-     * Starts background animation for lost password activity.
+    /**
+     * Starts background animation for activity.
      */
     private void initBackground(){
-        // Initialise values.
-        mLostPasswordBackground = findViewById(R.id.lost_password_background);
-        mBackgroundAnimation = (AnimationDrawable) mLostPasswordBackground.getBackground();
-        // Set duration.
-        mBackgroundAnimation.setEnterFadeDuration(4500);
-        mBackgroundAnimation.setExitFadeDuration(4500);
-        //Start animation.
-        mBackgroundAnimation.start();
+        ConstraintLayout background = findViewById(R.id.login_background);
+        AnimationDrawable animationDrawable = (AnimationDrawable) background.getBackground();
+
+        // Duration.
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        //Start.
+        animationDrawable.start();
     }
 }

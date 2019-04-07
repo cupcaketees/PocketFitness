6caac520485863dc0ddfcb69991e79a855f1cd9d
Sam-Hammersley <q5315908@live.tees.ac.uk>
2019-04-07 20:47:10 +0100

    Make changes to SensorAdapter

--- app/src/main/java/uk/ac/tees/cupcake/home/steps/StepCounterService.java
+++ app/src/main/java/uk/ac/tees/cupcake/home/steps/StepCounterService.java
@@ -11,19 +11,18 @@ import uk.ac.tees.cupcake.sensors.SensorAdapter;
 import uk.ac.tees.cupcake.sensors.StepCounterSensorListener;
 
 /**
+ * A started non-bound service which registers the step counter listener to listen for step count events.
+ *
  * @author Sam-Hammersley <q5315908@tees.ac.uk>
  */
-
 public final class StepCounterService extends Service {
     
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        SensorAdapter sensorAdapter = new SensorAdapter(getApplicationContext(), Sensor.TYPE_STEP_COUNTER);
+        SensorAdapter sensorAdapter = new SensorAdapter(getApplicationContext());
         StepCounterSensorListener eventListener = new StepCounterSensorListener(getApplicationContext());
     
-        if (sensorAdapter.setupSensors()) {
-            sensorAdapter.registerListener(SensorManager.SENSOR_DELAY_NORMAL, Sensor.TYPE_STEP_COUNTER, eventListener);
-        }
+        sensorAdapter.addSensorWithListener(Sensor.TYPE_STEP_COUNTER, SensorManager.SENSOR_DELAY_NORMAL, eventListener);
         
         return Service.START_STICKY;
     }

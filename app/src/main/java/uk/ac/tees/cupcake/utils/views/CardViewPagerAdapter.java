aa5e88fd4d112280ac720ce4c74699c36ca17a4b
Sam-Hammersley <q5315908@live.tees.ac.uk>
2019-04-23 04:57:23 +0100

    Add onclicklisteners to exercise cards for selection

--- app/src/main/java/uk/ac/tees/cupcake/utils/views/CardViewPagerAdapter.java
+++ app/src/main/java/uk/ac/tees/cupcake/utils/views/CardViewPagerAdapter.java
@@ -1,5 +1,7 @@
 package uk.ac.tees.cupcake.utils.views;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
@@ -11,25 +13,20 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.List;
-
 import uk.ac.tees.cupcake.R;
-import uk.ac.tees.cupcake.home.health.Exercise;
+import uk.ac.tees.cupcake.home.health.exercise.Exercise;
+import uk.ac.tees.cupcake.home.health.exercise.ExerciseMapActivity;
 
 /**
  * @author Sam-Hammersley <q5315908@tees.ac.uk>
  */
 public final class CardViewPagerAdapter extends PagerAdapter {
 
-    private final List<Exercise> exercises;
-
-    public CardViewPagerAdapter(List<Exercise> exercises) {
-        this.exercises = exercises;
-    }
-
+    private final Exercise[] exercises = Exercise.values();
+    
     @Override
     public int getCount() {
-        return exercises.size();
+        return exercises.length;
     }
     
     @Override
@@ -45,20 +42,27 @@ public final class CardViewPagerAdapter extends PagerAdapter {
         
         CardView cardView = view.findViewById(R.id.cardView);
         bind(cardView, position);
-        
+
         return cardView;
     }
     
     private void bind(View cardView, int position) {
+        cardView.setEnabled(true);
         TextView titleTextView = cardView.findViewById(R.id.exercise_selection_item_title);
         ImageView imageView = cardView.findViewById(R.id.exercise_selection_item_image_view);
         
-        Exercise exercise = exercises.get(position);
+        Exercise exercise = exercises[position];
         Drawable drawable = ContextCompat.getDrawable(cardView.getContext(), exercise.getDrawableId());
         
-        titleTextView.setText(exercise.getTitle());
+        titleTextView.setText(exercise.getName());
         imageView.setImageDrawable(drawable);
-        imageView.setColorFilter(0xFF757575, PorterDuff.Mode.SRC_ATOP);
+        imageView.setColorFilter(0xFFFFFFFF, PorterDuff.Mode.SRC_ATOP);
+        
+        cardView.setOnClickListener(v -> {
+            Context context = v.getContext();
+    
+            context.startActivity(new Intent(context, ExerciseMapActivity.class).putExtra("exercise", exercise));
+        });
     }
 
 }
